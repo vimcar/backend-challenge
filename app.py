f0d21c6d6391eb42d6e99fe86605c782888e2e4e
barbaradimitrova <barbara.dimitrova@goeuro.com>
2017-12-31 15:25:23 +0100

    version 1 - simple db for users, confirm email and token creation

---
+++ app.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+import os
+
+import datetime
+from flask import Flask, abort, request, jsonify, g, url_for, flash, render_template
+#from flask_login import login_required
+from flask_mail import Message, Mail
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired, URLSafeTimedSerializer)
+
+
+# initialization
+from validate_email import validate_email
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+mail = Mail(app)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['N_TOKENS'] = 1
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def verify_email(email):
+        return validate_email(email,check_mx=True)
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username, password):
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    if not username.verify_email(username):
+        abort(400)   # not valid email
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
+@app.route("/")
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
+
+
+@app.route('/send')
+def send_confirmation(email):
+    token = generate_confirmation_token(email)
+    confirm_url = url_for('user.confirm_email', token=token, _external=True)
+
+    html = "<p>Welcome! Thanks for signing up. Please follow this link to activate your account:</p>\
+    <p><a href={}</a></p>\
+    <br>\
+    <p>Cheers!</p>".format(confirm_url)
+
+    subject = "Please confirm your email"
+    send_email(email, subject, html)
+    flash('A confirmation email has been sent.', 'success')
+    return redirect(url_for('user.unconfirmed'))
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
+
+if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+    app.run(debug=True)
+
+
---
+++ config.py
@@ -0,0 +1,3 @@
+class BaseConfig():
+    SECRET_KEY = 'my_precious'
+    SECURITY_PASSWORD_SALT = 'my_precious_two'
